ef72927596f1c66fc6f5f8ea76e9ec676a8dc4ad
Danyveneno <danieledjv@gmail.com>
2023-01-02 08:05:32 -0600

    Configuraciòn de links de redes

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,17 +1,22 @@
 export const socials = [
   {
-    name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
-    icon: 'mdi:twitter'
+    name: "Twitter",
+    url: "https://twitter.com/VenenoDigital",
+    icon: "mdi:twitter"
   },
   {
-    name: 'GitHub',
-    url: 'https://github.com/littlesticks',
-    icon: 'mdi:github'
+    name: "GitHub",
+    url: "https://github.com/DanyVeneno",
+    icon: "mdi:github"
   },
   {
-    name: 'Instagram',
-    url: 'https://instagram.com',
-    icon: 'mdi:instagram'
+    name: "Instagram",
+    url: "https://www.instagram.com/juanitovenenoestudio/",
+    icon: "mdi:instagram"
+  },
+  {
+    name: "Youtube",
+    url: "https://www.youtube.com/channel/UC8UhdMAKJX56O2PY8kzBIlw",
+    icon: "mdi:youtube"
   }
-]
+];
