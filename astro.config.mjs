663534154b1113810a175a8ae026a0a0c097ce58
Danyveneno <danieledjv@gmail.com>
2023-01-02 06:51:32 -0600

    configurando astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
-import { settings } from './src/data/settings';
+import { defineConfig } from "astro/config";
+import { settings } from "./src/data/settings";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   site: settings.site,
   integrations: [sitemap(), mdx()],
   vite: {
