28a564bbdd75154382f4139f7d3e9e9d8e20b526
Danyveneno <danieledjv@gmail.com>
2023-02-11 15:16:13 -0600

    rss2

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,9 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 export async function get(context) {
-  const blog = await getCollection("blog");
   return rss({
-    title: "Buzz’s Blog",
-    description: "A humble Astronaut’s guide to the stars",
+    title: "Venenews",
+    description: "Crear Para Generar",
     site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      customData: post.data.customData,
-      // Calcula el link RSS desde el `slug` del post
-      // Este ejemplo asume que todos los posts son renderizados como rutas `/blog/[slug]`
-      link: `/blog/${post.slug}/`
-    }))
+    items: await pagesGlobToRssItems(import.meta.glob("./blog/*.{md,mdx}"))
   });
 }
